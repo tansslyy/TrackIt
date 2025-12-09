764aa397479c02242ae26abd19e46713a6ec782f
tansslyy <2501tsaul@gmail.com>
2025-12-09 23:04:39 +0200

    refactor: remove user-decorator

--- server/src/modules/users/user.controller.ts
+++ server/src/modules/users/user.controller.ts
@@ -1,13 +1,6 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { JwtGuard } from '../../security/guards';
 import { User } from 'generated/prisma';
-import { GetUser } from '../auth/decorator';
 
 @Controller('users')
-export class UserController {
-  @UseGuards(JwtGuard)
-  @Get('me')
-  getme(@GetUser() user: User) {
-    return user;
-  }
-}
+export class UserController {}
