589c74e59fbd03dc64a8182b41cbd2e24d5e9c1d
tansslyy <2501tsaul@gmail.com>
2026-02-09 13:02:51 +0200

    Save file

--- client/src/pages/Home/GuestHome.tsx
+++ client/src/pages/Home/GuestHome.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import styles from "./HomePage.module.css";
-import { UserAvatar } from "../../assets/images/UserAvatar";
+import { UserAvatar } from "../../assets/images/UserAvatar/UserAvatar";
 
 interface GuestHomeProps {
   t: any;
